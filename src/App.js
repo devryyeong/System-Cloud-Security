568757ebac78663a72096bc27b3a169a2f89c738
devryyeong <thisisme20@daum.net>
2022-07-07 23:45:37 +0900

    :adhesive_bandage: 불필요한 파일 삭제
    chore: 불필요한 파일 삭제

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Count from './Count';
 
-function App() {
+function App(props) {
+
+  const [count, setCount] = useState(0);
+
+  const countPlus = () => {
+    if(count<20) {
+      setCount(count + 1);
+    }
+  }
+
+  const countMinus = () => {
+    if(count > 0) {
+      setCount(count - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>220705 CaseStudy 2주차 React Intro. Counter 실습</p>
+      <p>{count}</p>
+      <button onClick={countPlus}>+</button>
+      <button onClick={countMinus}>-</button>
+      <p>Props:</p>
+      <Count style={{ backgroundColor: 'blue' }}></Count>
+      <p style={{ backgroundColor: 'yellow' }}>{props.count}dd</p>
     </div>
   );
 }
 
 export default App;
+
